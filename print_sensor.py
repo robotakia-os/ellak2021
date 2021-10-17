d56d0a8cbe8396af8e6968009538012f27b3a099
robotakia-os <80763756+robotakia-os@users.noreply.github.com>
2021-10-17 11:21:05 +0300

    Delete print_sensor.py

--- print_sensor.py
+++
@@ -1,8 +0,0 @@
-from machine import Pin, ADC
-from time import sleep
-
-sensor = ADC(Pin(26))
-
-while True:
-    print(sensor.read_u16()) # μη προσημασμένη τιμή 16bit (εύρος: 0-65535)
-    sleep(0.5)
